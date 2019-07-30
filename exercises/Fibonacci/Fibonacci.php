e5c2e5cad250a3529bde73f38908d2d738445f29
rzhetenov <zhetenov.r@rahmetapp.kz>
2019-07-30 10:53:08 +0600

    fibonacci implementation

--- exercises/Fibonacci/Fibonacci.php
+++ exercises/Fibonacci/Fibonacci.php
@@ -4,22 +4,77 @@ declare(strict_types=1);
 
 namespace Exercises\Fibonacci;
 
+use InvalidArgumentException;
+
 /**
  * The fibonacci series is a series of numbers where
  * each consecutive number is the sum of the previous two.
  * For example [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ∞]
  *
- * @method static int get(int $index)
- * @example Fibonacci::get(7) === 13
  */
 class Fibonacci
 {
+
     /**
-     * Fibonacci constructor.
-     * @param int $value
+     * @var $cache
      */
-    public function __construct(int $value)
+    protected $cache = [];
+
+    /**
+     * Recursion means calling functions directly from itself.
+     * You must determine the number n is negative or positive. If the number is negative, then you need to raise the error.
+     * After that you need to check if this number is in the cache. If this number in the cache return this number.
+     * If not, you need to recursively find (n - 1) + (n - 2).
+     * Until n equals one or zero.
+     *
+     * @param int $value
+     * @return int
+     */
+    public function recursiveImplementation(int $value): int
     {
-        // TODO: here your code
+        if ($value < 0) {
+            throw new InvalidArgumentException('Work only with positive numbers.');
+        }
+
+         if ($value === 0) {
+             return 0;
+         }
+         elseif ($value === 1 || $value === 2) {
+             return 1;
+         }
+         else {
+            if (!(!is_null($this->cache) && array_key_exists($value, $this->cache))) {
+                $this->cache[$value] = $this->recursiveImplementation($value-1) + $this->recursiveImplementation($value-2);
+            }
+
+            return $this->cache[$value];
+        }
+    }
+
+    /**
+     * Dynamic programming is a way to solve complex problems, breaking them down into simpler sub-tasks.
+     * You must determine the number n is negative or positive. If the number is negative, then you need to raise the error.
+     * After that you need to create two initial variables which is equal to 0 and 1 or you can use dictionary created above.
+     * Now you need for one iteration to calculate the Fibonacci number using this formula [(i - 1) + (i - 2)] using previously created variables.
+     *
+     * @param int $value
+     * @return int
+     */
+    public function dynamicImplementation(int $value):int
+    {
+        if ($value < 0) {
+            throw new InvalidArgumentException('Work only with positive numbers.');
+        }
+
+        $this->cache[0] = 0;
+
+        if ($value > 0) {
+            $this->cache[1] = 1;
+            for ($i = 2; $i < $value + 1; $i++) {
+                $this->cache[$i] = $this->cache[$i - 1] + $this->cache[$i - 2];
+            }
+        }
+
+        return $this->cache[$value];
     }
 }
--- tests/Fibonacci/FibonacciTest.php
+++ tests/Fibonacci/FibonacciTest.php
@@ -5,17 +5,72 @@ declare(strict_types=1);
 namespace Tests\Fibonacci;
 
 use Exercises\Fibonacci\Fibonacci;
+use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 
 class FibonacciTest extends TestCase
 {
-    public function testZeroValue(): void
+
+    public function testRecursiveImplementationWithZeroValue(): void
     {
-        $this->assertSame(0, new Fibonacci(0));
+        $fib = new Fibonacci();
+        $result = $fib->recursiveImplementation(0);
+        $this->assertSame(0, $result);
     }
 
-    public function testOne(): void
+    public function testDynamicImplementationWithZeroValue(): void
     {
-        $this->assertSame(1, new Fibonacci(1));
+        $fib = new Fibonacci();
+        $result = $fib->dynamicImplementation(0);
+        $this->assertSame(0, $result);
+    }
+
+    public function testRecursiveImplementationWithOneValue(): void
+    {
+        $fib = new Fibonacci();
+        $result =  $fib->recursiveImplementation(1);
+        $this->assertSame(1, $result);
+    }
+
+    public function testDynamicImplementationWithOneValue(): void
+    {
+        $fib = new Fibonacci();
+        $result = $fib->dynamicImplementation(1);
+        $this->assertSame(1, $result);
+    }
+
+    public function testDynamicImplementationWithNormalValue(): void
+    {
+        $fib = new Fibonacci();
+        $result = $fib->dynamicImplementation(34);
+        $this->assertSame(5702887, $result);
+    }
+
+    public function testRecursiveImplementationWithNormalValue(): void
+    {
+        $fib = new Fibonacci();
+        $result = $fib->recursiveImplementation(34);
+        $this->assertSame(5702887, $result);
+    }
+
+
+    /**
+     * @expectedException InvalidArgumentException
+     * @expectedExceptionMessage Work only with positive numbers.
+     */
+    public function testRecursiveImplementationWithNegativeNumber(): void
+    {
+        $fib = new Fibonacci();
+        $fib->recursiveImplementation(-1);
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     * @expectedExceptionMessage Work only with positive numbers.
+     */
+    public function testDynamicImplementationWithNegativeNumber(): void
+    {
+        $fib = new Fibonacci();
+        $fib->dynamicImplementation(-1);
     }
 }
